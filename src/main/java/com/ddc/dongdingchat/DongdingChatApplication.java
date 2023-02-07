9b5252067f91138574e13f19e9761ff6f54a383b
DongJu-Na <ehdeld123@naver.com>
2023-02-07 16:51:36 +0900

    init
    init

---
+++ src/main/java/com/ddc/dongdingchat/DongdingChatApplication.java
@@ -0,0 +1,74 @@
+package com.ddc.dongdingchat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.UnicastProcessor;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+
+@SuppressWarnings("deprecation")
+@SpringBootApplication
+public class DongdingChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongdingChatApplication.class, args);
+	}
+	
+  @Bean
+  public UnicastProcessor<Event> eventPublisher(){
+      return UnicastProcessor.create();
+  }
+  
+  @Bean
+  public Flux<Event> events(UnicastProcessor<Event> eventPublisher) {
+      return eventPublisher
+              .replay(25)
+              .autoConnect();
+  }
+
+  @Bean
+  public RouterFunction<ServerResponse> routes(){
+      return RouterFunctions.route(
+              GET("/"),
+              request -> ServerResponse.ok().body(BodyInserters.fromResource(new ClassPathResource("public/index.html")))
+      );
+  }
+
+  @Bean
+  public HandlerMapping webSocketMapping(UnicastProcessor<Event> eventPublisher, Flux<Event> events) {
+      Map<String, Object> map = new HashMap<>();
+      map.put("/websocket/chat", new ChatSocketHandler(eventPublisher, events));
+      SimpleUrlHandlerMapping simpleUrlHandlerMapping = new SimpleUrlHandlerMapping();
+      simpleUrlHandlerMapping.setUrlMap(map);
+
+      //Without the order things break :-/
+      simpleUrlHandlerMapping.setOrder(10);
+      return simpleUrlHandlerMapping;
+  }
+
+  @Bean
+  public WebSocketHandlerAdapter handlerAdapter() {
+      return new WebSocketHandlerAdapter();
+  }
+
+  @Bean
+  public UserStats userStats(Flux<Event> events, UnicastProcessor eventPublisher){
+      return new UserStats(events, eventPublisher);
+  }
+
+}
---
+++ src/test/java/com/ddc/dongdingchat/DongdingChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddc.dongdingchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongdingChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
