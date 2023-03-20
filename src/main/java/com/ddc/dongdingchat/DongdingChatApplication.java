008d2f50a6b4d84cc90b4f852ff76bdc414b62ea
DongJu-Na <ehdeld123@naver.com>
2023-03-20 10:15:59 +0900

    Update DongdingChatApplication.java

--- src/main/java/com/ddc/dongdingchat/DongdingChatApplication.java
+++ src/main/java/com/ddc/dongdingchat/DongdingChatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DongdingchatApplication {
+public class DongdingChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DongdingchatApplication.class, args);
+		SpringApplication.run(DongdingChatApplication.class, args);
 	}
 
 }
