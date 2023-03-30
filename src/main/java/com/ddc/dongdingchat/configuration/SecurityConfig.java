b9c0545593602bf8d8f2a78abca3f481f1f8b1c1
DongJu-Na <ehdeld123@naver.com>
2023-03-30 18:05:25 +0900

    230330
    시큐리티 설정, api컨트롤러 추가예정

--- src/main/java/com/ddc/dongdingchat/configuration/SecurityConfig.java
+++ src/main/java/com/ddc/dongdingchat/configuration/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.ddc.dongdingchat.configuration;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Configuration
@@ -28,14 +33,19 @@ public class SecurityConfig {
 	}
 	
   @Bean
-  public MapReactiveUserDetailsService userDetailsService() {
-      UserDetails user = User.withDefaultPasswordEncoder()
-          .username("user")
-          .password("user")
-          .roles("USER")
-          .build();
-      return new MapReactiveUserDetailsService(user);
+  public MapReactiveUserDetailsService userDetailsRepository() {
+      User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+      UserDetails user = userBuilder.username("user").password("user").roles("USER").build();
+      UserDetails admin = userBuilder.username("admin").password("admin").roles("USER", "ADMIN").build();
+      return new MapReactiveUserDetailsService(user, admin);
   }
+  
+  private Mono<AuthorizationDecision> currentUserMatchesPath(Mono<Authentication> authentication, AuthorizationContext context) {
+    return authentication
+            .map(a -> context.getVariables().get("user").equals(a.getName()))
+            .map(granted -> new AuthorizationDecision(granted));
+  }
+
   
 
 	@Bean
