08785b433e1044ffc2580039bb6cdee1e043f219
DongJu-Na <ehdeld123@naver.com>
2023-03-30 18:52:37 +0900

    authcontroller 수정
    init

--- src/main/java/com/ddc/dongdingchat/controller/AuthController.java
+++ src/main/java/com/ddc/dongdingchat/controller/AuthController.java
@@ -1,29 +1,52 @@
 package com.ddc.dongdingchat.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ddc.dongdingchat.configuration.jwt.JwtTokenProvider;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Validated
 public class AuthController {
 	/*
-	  private final SecurityService securityService;
+  private final JwtTokenProvider tokenProvider;
 
-    public AuthController(SecurityService securityService) {
-        this.securityService = securityService;
-    }
+  private final ReactiveAuthenticationManager authenticationManager;
 
-    @PostMapping("/login")
-    public Mono<ResponseEntity<Object>> login(@RequestBody UserLoginDto dto) {
-        return securityService.authenticate(dto.getUsername(), dto.getPassword())
-                .flatMap(tokenInfo -> Mono.just(ResponseEntity.ok(AuthResultDto.builder()
-                        .userId(tokenInfo.getUserId())
-                        .token(tokenInfo.getToken())
-                        .issuedAt(tokenInfo.getIssuedAt())
-                        .expiresAt(tokenInfo.getExpiresAt())
-                        .build())));
-    }
-	*/
+  @PostMapping("/login")
+  public Mono<ResponseEntity> login(
+          @Valid @RequestBody Mono<AuthenticationRequest> authRequest) {
+
+      return authRequest
+              .flatMap(login -> this.authenticationManager
+                      .authenticate(new UsernamePasswordAuthenticationToken(
+                              login.getUsername(), login.getPassword()))
+                      .map(this.tokenProvider::createToken))
+              .map(jwt -> {
+                  HttpHeaders httpHeaders = new HttpHeaders();
+                  httpHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + jwt);
+                  var tokenBody = Map.of("access_token", jwt);
+                  return new ResponseEntity<>(tokenBody, httpHeaders, HttpStatus.OK);
+              });
+
+  }
+  */
 	
 }
